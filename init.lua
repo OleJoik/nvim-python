a308f6a979afcc92a9f623655a26c7072dd2162e
Ole-Jakob Olsen <ole.jakob.olsen@ngi.no>
2023-07-06 21:59:17 +0200

    Lazygit, telescope, copilot

--- init.lua
+++ init.lua
@@ -1,1 +1,34 @@
-print("Hello world")
+vim.g.mapleader = " "
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+require("lazy").setup({
+"github/copilot.vim",
+    {
+        "kdheepak/lazygit.nvim",
+        -- optional for floating window border decoration
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+        },
+    },
+    --install telescope
+{
+    'nvim-telescope/telescope.nvim', tag = '0.1.2',
+-- or                              , branch = '0.1.x',
+      dependencies = { 'nvim-lua/plenary.nvim' }
+    }
+})
+
+require("remap")
+require("plugins")
+
---
+++ remap.lua
@@ -0,0 +1,3 @@
+
+vim.api.nvim_set_keymap('n', '<leader>lg', ':LazyGit<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>pv', ':Ex<CR>', { noremap = true, silent = true })
