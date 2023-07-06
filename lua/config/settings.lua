26b329a7a2d67f338ce0cd7c216b409fe92fa43f
Ole-Jakob Olsen <ole.jakob.olsen@ngi.no>
2023-07-07 01:30:16 +0200

    WIP Python LSP

--- lua/config/settings.lua
+++ lua/config/settings.lua
@@ -1,12 +1,14 @@
-
 -- create a setting to show relative line numbers
-vim.api.nvim_command('set relativenumber')
+vim.api.nvim_command("set relativenumber")
 
 -- also show absolute line numbers
-vim.api.nvim_command('set number')
+vim.api.nvim_command("set number")
 
 -- set the tab width to 4 spaces
-vim.api.nvim_command('set tabstop=4')
+vim.api.nvim_command("set tabstop=4")
 
 -- set the width of an indent to 4 spaces
-vim.api.nvim_command('set shiftwidth=4')
+vim.api.nvim_command("set shiftwidth=4")
+
+-- hide netrw banner
+vim.api.nvim_command("let g:netrw_banner = 0")
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,5 +1,49 @@
-local lsp = require('lsp-zero').preset({})
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities.textDocument.completion.completionItem = {
+	documentationFormat = { "markdown", "plaintext" },
+	snippetSupport = true,
+	preselectSupport = true,
+	insertReplaceSupport = true,
+	labelDetailsSupport = true,
+	deprecatedSupport = true,
+	commitCharactersSupport = true,
+	tagSupport = { valueSet = { 1 } },
+	resolveSupport = {
+		properties = {
+			"documentation",
+			"detail",
+			"additionalTextEdits",
+		},
+	},
+}
 
+local on_attach = function(ev)
+	-- Enable completion triggered by <c-x><c-o>
+	vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
+
+	-- Buffer local mappings.
+	-- See `:help vim.lsp.*` for documentation on any of the below functions
+	local opts = { buffer = ev.buf }
+	vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+	vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+	vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+	vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+	vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
+	vim.keymap.set("n", "<space>wa", vim.lsp.buf.add_workspace_folder, opts)
+	vim.keymap.set("n", "<space>wr", vim.lsp.buf.remove_workspace_folder, opts)
+	vim.keymap.set("n", "<space>wl", function()
+		print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+	end, opts)
+	vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, opts)
+	vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, opts)
+	vim.keymap.set({ "n", "v" }, "<space>ca", vim.lsp.buf.code_action, opts)
+	vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+	vim.keymap.set("n", "<space>f", function()
+		vim.lsp.buf.format({ bufnr = opts.buffer })
+	end, opts)
+end
+
+local lsp = require("lsp-zero").preset({})
 
 lsp.on_attach(function(_, bufnr)
 	lsp.default_keymaps({ buffer = bufnr })
@@ -7,19 +51,29 @@ end)
 
 lsp.setup()
 
-require 'lspconfig'.pyright.setup {}
-require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
+require("lspconfig").pyright.setup({
+	on_attach = on_attach,
+	capabilities = capabilities,
+	settings = {
+		python = {
+			analysis = {
+				autoSearchPaths = true,
+				useLibraryCodeForTypes = true,
+			},
+		},
+	},
+})
+require("lspconfig").lua_ls.setup(lsp.nvim_lua_ls())
+require("lspconfig").ruff_lsp.setup({})
 
 local null_ls = require("null-ls")
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 null_ls.setup({
 	sources = {
+		null_ls.builtins.diagnostics.mypy,
 		null_ls.builtins.formatting.stylua,
-		null_ls.builtins.diagnostics.eslint,
-		null_ls.builtins.completion.spell,
-		null_ls.builtins.formatting.black
+		null_ls.builtins.formatting.black,
 	},
-	-- you can reuse a shared lspconfig on_attach callback here
 	on_attach = function(client, bufnr)
 		if client.supports_method("textDocument/formatting") then
 			vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
@@ -32,4 +86,18 @@ null_ls.setup({
 			})
 		end
 	end,
+})
+
+-- Global mappings.
+-- See `:help vim.diagnostic.*` for documentation on any of the below functions
+vim.keymap.set("n", "<space>e", vim.diagnostic.open_float)
+vim.keymap.set("n", "[d", vim.diagnostic.goto_prev)
+vim.keymap.set("n", "]d", vim.diagnostic.goto_next)
+vim.keymap.set("n", "<space>q", vim.diagnostic.setloclist)
+
+-- Use LspAttach autocommand to only map the following keys
+-- after the language server attaches to the current buffer
+vim.api.nvim_create_autocmd("LspAttach", {
+	group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+	callback = on_attach,
 })
